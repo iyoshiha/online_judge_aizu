7f956e6091c9b6d7c715b60431dd6c073bd15fbe
iyoshiha <study.e.jp@gmail.com>
2021-03-23 12:11:51 +0900

    fixed git corruption

--- alds1_x/selection_sort/main.c
+++ alds1_x/selection_sort/main.c
@@ -2,6 +2,12 @@
 # include <stdlib.h>
 # include <string.h>
 
+typedef struct s_data
+{
+	int index;
+	int minv;
+} t_data;
+
 void trace(int *nums, int n)
 {
 	for (int p = 0; n > p; p++)
@@ -25,4 +31,59 @@ int getNumArray(int **nums, int *n)
 		i++;
 	}
 	return (1);
+}
+
+/*
+   int linerSearch(int num, int n, const int doneindex, int *minv)
+{
+	int i;
+
+	*minv = num[doneindex];
+	i = doneindex;
+	while (n > i + 2)
+	{
+		if (*minv > num[i + 1])
+			*minv = num[i + 1];
+	}
+	return (doneindex + 1);
+}
+*/
+int main()
+{
+	int n;
+	int *num;
+	int minv;
+	int i;
+	int di;
+	t_data minval;
+
+	di = -1;
+	getNumArray(&num, &n);
+
+	while (++di < n - 1)
+	{
+		i = di;
+		minval.minv = num[di];
+		while (n > i)
+		{
+			if (minval.minv > num[i])
+			{
+				minval.minv = num[i];
+				minval.index = i;
+			}
+			i++;
+		}
+		if (minval.minv == num[di])
+			continue;
+		i = minval.index;
+		while ((i > di )) // swap all;
+		{
+			num[i] = num[i - 1];
+			i--;
+
+		}
+		num[di] = minval.minv;
+	}
+	trace(num, n);
+	return (0);
 }
