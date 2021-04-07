409ba98c2dd95bcc728a979675485ed9aeb3be04
iyoshiha <study.e.jp@gmail.com>
2021-04-07 23:35:54 +0900

    add push

--- data_structrure/stack.c
+++ data_structrure/stack.c
@@ -3,6 +3,9 @@
 # include <stdlib.h>
 # include <string.h>
 
+int	top;
+int *S; // Stack;
+
 int initTop()
 {
 
@@ -19,21 +22,23 @@ int	isFull(int top, int num)
 	return (top >= num - 1);
 }
 
-int	push()
+int	push(int num)
 {
-
-
+	S[++top] = num;
 }
 
 int	pop()
 {
-
-
+	if isEmpty()
+		// error
+	top--;
+	return S[top+1];
 }
 
 int	main(void)
 {
 	
+
 
 	return(0);
 }
