8fc83d526fb71dd2e853e65b44c5913c03dd7338
iyoshiha <study.e.jp@gmail.com>
2021-04-08 23:45:49 +0900

    add is_digit()

--- data_structrure/stack.c
+++ data_structrure/stack.c
@@ -9,6 +9,20 @@
 int	top; // this points at the last of elements of S;
 int S[MAX]; // Stack;
 
+int	is_digit(int n)
+{
+	char num[11] "123456789";
+	int i;
+
+	while (num[i] == '\0')
+	{
+	if (n == num[i])
+		return (1);
+	i++;
+	}
+	else return (0);
+}
+
 int initTop()
 {
 	top = 0;
@@ -42,9 +56,19 @@ int	pop()
 	return (S[top+1]);
 }
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
-	initTop();	
+	int i = 0;
+	initTop();
+	
+	while (argc  > ++i)
+	{
+		if (is_digit())
+			push(argv[i][0]);
+		else if (is_operator())
+
+
+	}
 
 	return(0);
 }
